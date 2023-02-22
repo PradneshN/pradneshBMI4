f6c70d06020b956394873fcd2806c31a039191ea
PradneshN <100787192+PradneshN@users.noreply.github.com>
2023-02-22 18:32:23 +0530

    Add files via upload

---
+++ Bmi.py
@@ -0,0 +1,3 @@
+name = input("Hello Sir/Mam! Please enter your name to start calculating your BMI (Body Mass Index)! : ")
+wieght = float(input("Please enter you weight! : "))
+height = float(input("Please enter your height in feet! : "))
